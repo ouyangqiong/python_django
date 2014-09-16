f924cf26ca0a02ddaed479998fd929666c9c5235
eqioouy <qiong.ouyang@ericsson.com>
2014-09-16 18:12:30 +0800

    upload first

---
+++ src/news/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('pub_date', models.DateField()),
+                ('headline', models.CharField(max_length=200)),
+                ('content', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Reporter',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('full_name', models.CharField(max_length=70)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='reporter',
+            field=models.ForeignKey(to='news.Reporter'),
+            preserve_default=True,
+        ),
+    ]
