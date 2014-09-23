677813c8820d6941604092205be562d22a901351
Zhaoxi <718057091@qq.com>
2014-09-23 23:36:58 +0800

    add url

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -8,4 +8,9 @@ Created on Sep 22, 2014
 '''
 from django.http import HttpResponse
 
-urlpatterns= patterns('',url(r'^$',views.index,name='index'))
+urlpatterns= patterns('',
+    url(r'^$',views.index,name='index'),
+    url(r'^(?P<question_id>\d+)/$',views.detail,name="detail"),
+    url(r'^(?P<question_id>)\d+/vote/$',views.vote,name="vote"),
+    url(r'^(?P<question_id>)\d+/results/$',views.results,name="results"),
+)
