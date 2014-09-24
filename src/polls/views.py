da30964157a5c56f5cc485b706f736192937da94
eqioouy <qiong.ouyang@ericsson.com>
2014-09-24 16:55:49 +0800

    optimize views

--- src/polls/views.py
+++ src/polls/views.py
@@ -4,14 +4,18 @@ from django.template import RequestContext, loader
 from polls.models import Question
 
 # Create your views here.
+# def index(request):
+#     latest_question_list=Question.objects.order_by("-pub_date")[:5]
+#     #output=', '.join([p.question_text for p in latest_question_list])
+#     template=loader.get_template('polls/index.html')
+#     context=RequestContext(request,{
+#         'latest_question_list':latest_question_list,                 
+#     })
+#     return HttpResponse(template.render(context))
 def index(request):
     latest_question_list=Question.objects.order_by("-pub_date")[:5]
-    #output=', '.join([p.question_text for p in latest_question_list])
-    template=loader.get_template('polls/index.html')
-    context=RequestContext(request,{
-        'latest_question_list':latest_question_list,                 
-    })
-    return HttpResponse(template.render(context))
+    ditionary={'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',ditionary)
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question %s." % question_id)
