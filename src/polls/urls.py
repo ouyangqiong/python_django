f6febd8a24f9a218b9e84ab0c750b06987516981
eqioouy <qiong.ouyang@ericsson.com>
2014-10-10 17:49:41 +0800

    add vote page

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -11,6 +11,6 @@ from django.http import HttpResponse
 urlpatterns= patterns('',
     url(r'^$',views.index,name='index'),
     url(r'^(?P<question_id>\d+)/$',views.detail,name="detail"),
-    url(r'^(?P<question_id>)\d+/vote/$',views.vote,name="vote"),
-    url(r'^(?P<question_id>)\d+/results/$',views.results,name="results"),
+    url(r'^(?P<question_id>\d+)/vote/$',views.vote,name="vote"),
+    url(r'^(?P<question_id>\d+)/results/$',views.results,name="results"),
 )
