450b07e8743a858ab02daa2b037d23a859cf4b95
eqioouy <qiong.ouyang@ericsson.com>
2014-10-13 13:42:06 +0800

    add testcase

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -9,8 +9,8 @@ Created on Sep 22, 2014
 from django.http import HttpResponse
 
 urlpatterns= patterns('',
-    url(r'^$',views.index,name='index'),
-    url(r'^(?P<question_id>\d+)/$',views.detail,name="detail"),
+    url(r'^$',views.IndexView.as_view(),name='index'),
+    url(r'^(?P<pk>\d+)/$',views.DetailView.as_view(),name="detail"),
     url(r'^(?P<question_id>\d+)/vote/$',views.vote,name="vote"),
-    url(r'^(?P<question_id>\d+)/results/$',views.results,name="results"),
+    url(r'^(?P<pk>\d+)/results/$',views.ResultsView.as_view(),name="results"),
 )
--- src/polls/views.py
+++ src/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import RequestContext,loader
 from polls.models import Question,Choice
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 # Create your views here.
 # def index(request):
@@ -13,10 +14,22 @@ from django.core.urlresolvers import reverse
 #         'latest_question_list':latest_question_list,                 
 #     })
 #     return HttpResponse(template.render(context))
-def index(request):
-    latest_question_list=Question.objects.order_by("-pub_date")[:5]
-    ditionary={'latest_question_list':latest_question_list}
-    return render(request,'polls/index.html',ditionary)
+# def index(request):
+#     latest_question_list=Question.objects.order_by("-pub_date")[:5]
+#     ditionary={'latest_question_list':latest_question_list}
+#     return render(request,'polls/index.html',ditionary)
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name='latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('pub_date')[:5]
+class DetailView(generic.DetailView):
+    model=Question
+    template_name='polls/detail.html'
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name='polls/result.html'
 
 # def detail(request,question_id):
 #     try:
@@ -25,13 +38,13 @@ def index(request):
 #         raise Http404
 #     #return HttpResponse("You're looking at question %s." % question_id)
 #     return render(request,'polls/detail.html',{'question':question})
-def detail(request,question_id):
-    question= get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/detail.html",{'question':question})
-    
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/result.html",{'question':question})
+# def detail(request,question_id):
+#     question= get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/detail.html",{'question':question})
+#     
+# def results(request,question_id):
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/result.html",{'question':question})
 
 def vote(request,question_id):
     p = get_object_or_404(Question,pk=question_id)
