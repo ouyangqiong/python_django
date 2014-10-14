0f7a01c9c5f361721ed1dd2f531836669f3cc3de
eqioouy <qiong.ouyang@ericsson.com>
2014-10-14 15:14:08 +0800

    add test about view

--- src/polls/views.py
+++ src/polls/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext,loader
 from polls.models import Question,Choice
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 # def index(request):
@@ -23,7 +24,11 @@ class IndexView(generic.ListView):
     context_object_name='latest_question_list'
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('pub_date')[:5]
+        #return Question.objects.order_by('pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        
 class DetailView(generic.DetailView):
     model=Question
     template_name='polls/detail.html'
